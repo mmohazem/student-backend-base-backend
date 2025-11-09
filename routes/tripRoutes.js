30b21b6a72d6a7c2e79b05349e7fabdde753f9cb
Mariam <mariamtabdelati@gmail.com>
2025-11-10 01:07:41 +0200

    added db implementation, hashing implementation, and jwt tokens

--- routes/tripRoutes.js
+++ routes/tripRoutes.js
@@ -4,13 +4,15 @@ const {
   retrieveAllTrips,
 } = require('../controllers/tripController.js');
 
+const { verifyToken, verifyAdmin } = require('../controllers/authController.js'); // import it
+
 const tripRouter = express.Router();
 
 // All trips
 tripRouter
   .route('/')
-  .post(createTrip)        // Add new trip
-  .get(retrieveAllTrips);  // Get all trips
+  .post(verifyAdmin, createTrip)        // Add new trip
+  .get(verifyToken, retrieveAllTrips);  // Get all trips for authenticated users
 
 
 module.exports = tripRouter;
